41f3671c8fba1ca5e811a6073ff5c2f1791450b3
Nguyen Tri Duc <145568749+triduccc@users.noreply.github.com>
2025-06-23 23:02:48 +0700

    little changes

--- app.py
+++ app.py
@@ -28,41 +28,43 @@ def percentage_distribution(scores):
             bins["<60"] += 1
     return bins     
 
-if uploaded_file:
-    # Read the file and delete the Nan value in the grade
-    df = pd.read_excel(uploaded_file)
-    scores = df["Grade"].dropna().astype(float).tolist()  
+def main():
+    if uploaded_file:
+        # Read the file and delete the Nan value in the grade
+        df = pd.read_excel(uploaded_file)
+        scores = df["Grade"].dropna().astype(float).tolist()  
 
-    if scores:
-        # Display some infomation
-        st.write("Total number of students:", len(scores))
-        st.write("Average grade:", round(calculate_average(scores), 2))
+        if scores:
+            # Display some infomation
+            st.write("Total number of students:", len(scores))
+            st.write("Average grade:", round(calculate_average(scores), 2))
 
-        # Distribute grade
-        dist = percentage_distribution(scores)
-        labels = list(dist.keys())
-        values = list(dist.values())
+            # Distribute grade
+            dist = percentage_distribution(scores)
+            labels = list(dist.keys())
+            values = list(dist.values())
 
-        # Draw the pie chart
-        fig, ax = plt.subplots(figsize=(3,3))
-        ax.pie(values, labels=labels, autopct="%1.1f%%", startangle=90)
-        ax.axis("equal")
-        plt.tight_layout(pad=0.1)
+            # Draw the pie chart
+            fig, ax = plt.subplots(figsize=(3,3))
+            ax.pie(values, labels=labels, autopct="%1.1f%%", startangle=90)
+            ax.axis("equal")
+            plt.tight_layout(pad=0.1)
 
-        # Save the chart as an image for high resolution
-        buf = io.BytesIO()
-        fig.savefig(buf, format="png", dpi=300)
-        buf.seek(0)
-        st.markdown("Grade distribution chart")
-        img = Image.open(buf)
-
-        # Make 3 cols, with the middle one wider
-        col1, col2, col3 = st.columns([1,2,1])
-        with col2:
-            st.image(img, width=300)
+            # Save the chart as an image for high resolution
+            buf = io.BytesIO()
+            fig.savefig(buf, format="png", dpi=300)
+            buf.seek(0)
             st.markdown("Grade distribution chart")
-            
+            img = Image.open(buf)
 
+            # Make 3 cols, with the middle one wider
+            col1, col2, col3 = st.columns([1,2,1])
+            with col2:
+                st.image(img, width=300)
+                st.markdown("Grade distribution chart")
+
+if __name__ == "main":
+    main()
 
 
 
